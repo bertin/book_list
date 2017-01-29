712ab87b595798edbd90d68b510b993ad383d6ce
Bertin Hansen <bertin.hansen@makingwaves.no>
2017-01-29 18:38:32 +0100

    Updating to newer versions of react libraries and bootstrap

--- src/index.js
+++ src/index.js
@@ -13,3 +13,7 @@ ReactDOM.render(
     <App />
   </Provider>
   , document.querySelector('.container'));
+
+// We wire up Redux to our application by replacing the <App /> component by
+// an instance of the Provider component, giving it access to
+// the application state and reducers defined in the "./reducers" folder.
